6c6f710d4705bce567bb23d7bec9f32dc1039f3f
Movelocity <2814771302@qq.com>
2024-11-10 00:27:26 +0800

    调整配置文件

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-async redirects() {
-  return [
-    {
-      source: '/',
-      destination: '/list-files',
-      permanent: true, // Use `true` for a 308 permanent redirect, or `false` for a 307 temporary redirect
-    },
-  ];
-},
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/list-files',
+        permanent: true, // Use `true` for a 308 permanent redirect, or `false` for a 307 temporary redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
